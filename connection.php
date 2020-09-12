e3516c6ae76f97c81562450c767e4ced1f8348c5
thut4 <62836983+thut4@users.noreply.github.com>
2020-09-12 11:37:55 +0630

    Revert "initial commit"
    This reverts commit 07a0c2ef969b2b2f3ba81cb15321c9ca35fe35ae.

--- connection.php
+++
@@ -1,8 +0,0 @@
-<?php
-try {
-    $pdo = new PDO("mysql:dbname=food_delivery;host=localhost", 'root', '');
-} catch (PDOException $e) {
-    echo $e->getMessage();
-} catch (Exception $e) {
-    echo $e->getMessage();
-}
